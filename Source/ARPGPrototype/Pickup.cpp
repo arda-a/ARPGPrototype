c123199859fcfb2fd053056adb62dc5ce0746720
Arda Altay Abaka <6350204+arda-a@users.noreply.github.com>
2020-09-17 04:37:04 +0300

    feat: Basic pickups implementation

---
+++ Source/ARPGPrototype/Pickup.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pickup.h"
+
+APickup::APickup(){}
+
+void APickup::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+    Super::OnOverlapBegin(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+	UE_LOG(LogTemp, Warning, TEXT("Pickup:::OnOverlapBegin"));
+}
+
+void APickup::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+    Super::OnOverlapEnd(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex);
+	UE_LOG(LogTemp, Warning, TEXT("Pickup::OnOverlapEnd"));
+
+}
