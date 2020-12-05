b55fc16a8cb496957bf8c0be70cf861ccb2bd7e9
Arda A <arda.abaka@bordatech.com>
2020-12-05 20:44:40 +0300

    bugfix: Refining pickups - Pickups only possible by the character

--- Source/ARPGPrototype/Explosive.cpp
+++ Source/ARPGPrototype/Explosive.cpp
@@ -10,16 +10,14 @@ AExplosive::AExplosive()
 
 void AExplosive::OnOverlapBegin(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult)
 {
-    Super::OnOverlapBegin(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
-    UE_LOG(LogTemp, Warning, TEXT("Explosive::OnOverlapBegin"));
-
     if (OtherActor)
     {
         AMainCharacter *main = Cast<AMainCharacter>(OtherActor);
         if (main)
         {
+            Super::OnOverlapBegin(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
             main->DecrementHealth(Damage);
-
+            
             Destroy();
         }
     }
--- Source/ARPGPrototype/Pickup.cpp
+++ Source/ARPGPrototype/Pickup.cpp
@@ -10,14 +10,12 @@ APickup::APickup(){
 
 void APickup::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
 {
-    Super::OnOverlapBegin(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
-	UE_LOG(LogTemp, Warning, TEXT("Pickup:::OnOverlapBegin"));
-
     if (OtherActor)
     {
         AMainCharacter *main = Cast<AMainCharacter>(OtherActor);
         if (main)
         {
+            Super::OnOverlapBegin(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
             main->IncrementCoins(CoinCount);
             main->PickupLocations.Add(GetActorLocation());
             
